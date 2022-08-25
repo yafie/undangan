176b51c8d70704328f61fc5e0fc643e6e1b3d067
yafie <m.yafie.dalimunthe@gmail.com>
2022-08-26 01:30:10 +0700

    update undangan

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
-
   const queryString = window.location;
   const url = new URL(queryString);
   const untuk = url.searchParams.get("untuk");
-  
-   if (untuk) {
-     $(".mobile-title").html(untuk);
-     $("#nama").val(untuk);
+
+  if (untuk) {
+    $(".mobile-title").html(untuk);
+    $("#nama").val(untuk);
   }
 
   $(".instagram-effects").slick({
     dots: true,
-    slidesToShow: 2,
+    slidesToShow: 1,
     slidesToScroll: 1,
   });
 
-  $.ajax({    //create an ajax request to display.php
+  $.ajax({
+    //create an ajax request to display.php
     type: "GET",
     url: "./php/display.php",
-    dataType: "html",   //expect html to be returned                
+    dataType: "html", //expect html to be returned
     success: function (response) {
-      $( ".block-data-doa" ).append( response );
-    }
-  }); 
-        
+      $(".block-data-doa").append(response);
+    },
+  });
 });
 
 var x = document.getElementById("background_music");
@@ -37,24 +36,33 @@ function pauseAudio() {
 }
 
 $(".music-control").click(function () {
-  
-  var music = $(this).hasClass('off');
+  var music = $(this).hasClass("off");
 
   if (music) {
     playAudio();
-    $(this).removeClass('off');
-    $('.sound-off').hide();
-    $('.sound-on').show();
+    $(this).removeClass("off");
+    $(".sound-off").hide();
+    $(".sound-on").show();
   } else {
     pauseAudio();
-    $(this).addClass('off');
-    $('.sound-off').show();
-    $('.sound-on').hide();
+    $(this).addClass("off");
+    $(".sound-off").show();
+    $(".sound-on").hide();
   }
 });
 
 $("#pay").click(function () {
-  $("#id01").show();
+  $("#id01").fadeIn("slow");
+  gsap.from(".aniModal", {
+    scale: 0,
+    duration: 1.25,
+    opacity: 0,
+    ease: "circ.inOut",
+  });
+});
+
+$("#close-01").click(function () {
+  $("#id01").fadeOut("slow");
 });
 
 $("#okay").click(function () {
@@ -63,25 +71,38 @@ $("#okay").click(function () {
 
 $(".buka-udangan").click(function () {
   $(".main-title").hide();
-  $(".main-img").hide(); 
-  $("#pay").show(); 
+  $(".main-img").hide();
+  $("#pay").show();
   $(".desktop-bar").show();
   $(".separator").css("position", "absolute");
   $(".mobile-separator").css("visibility", "visible");
   $("#id02").show();
   playAudio();
-  $(".music-control").show().removeClass('off');
-  $('.sound-off').hide();
-  $('.sound-on').show();
+  $(".music-control").show().removeClass("off");
+  $(".sound-off").hide();
+  $(".sound-on").show();
 });
- 
-gsap.from(".aniTitle", { delay:0.5, scale: 0, duration: 1.25, opacity: 0, ease: "circ.inOut" });
+
+gsap.from(".aniTitle", {
+  delay: 0.5,
+  scale: 0,
+  duration: 1.25,
+  opacity: 0,
+  ease: "circ.inOut",
+});
+
+//WITH Timelines (cleaner, more versatile)
+var tl = gsap.timeline({ repeat: -1 });
+tl.to(".buka-udangan", { autoAlpha: 0, duration: 0.5 }),
+  tl.to(".buka-udangan", { autoAlpha: 1, duration: 1 }),
+  tl.to(".buka-udangan", { autoAlpha: 1, duration: 1 }),
+  tl.to(".buka-udangan", { autoAlpha: 0, duration: 0.5 });
 
 function animateFrom(elem, direction) {
   direction = direction || 1;
   var x = 0,
-      y = direction * 100;
-  if(elem.classList.contains("gs_reveal_fromLeft")) {
+    y = direction * 100;
+  if (elem.classList.contains("gs_reveal_fromLeft")) {
     x = -100;
     y = 0;
   } else if (elem.classList.contains("gs_reveal_fromRight")) {
@@ -90,82 +111,131 @@ function animateFrom(elem, direction) {
   }
   elem.style.transform = "translate(" + x + "px, " + y + "px)";
   elem.style.opacity = "0";
-  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-    duration: 2.75, 
-    x: 0,
-    y: 0, 
-    autoAlpha: 1, 
-    ease: "expo", 
-    overwrite: "auto"
-  });
+  gsap.fromTo(
+    elem,
+    { x: x, y: y, autoAlpha: 0 },
+    {
+      duration: 2.75,
+      x: 0,
+      y: 0,
+      autoAlpha: 1,
+      ease: "expo",
+      overwrite: "auto",
+    }
+  );
 }
 
 function hide(elem) {
-  gsap.set(elem, {autoAlpha: 0});
+  gsap.set(elem, { autoAlpha: 0 });
 }
 
 async function changeBackground(elemId, activeClass) {
   $(elemId).removeClass("active").addClass(activeClass);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   gsap.registerPlugin(ScrollTrigger);
-  
-  gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+
+  gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
     hide(elem); // assure that the element is hidden when scrolled into view
-    
+
     ScrollTrigger.create({
       trigger: elem,
       scroller: "#mobile-scroll",
-      onEnter: function() { animateFrom(elem) }, 
-      onEnterBack: function() { animateFrom(elem, -1) },
-      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+      onEnter: function () {
+        animateFrom(elem);
+      },
+      onEnterBack: function () {
+        animateFrom(elem, -1);
+      },
+      onLeave: function () {
+        hide(elem);
+      }, // assure that the element is hidden when scrolled into view
     });
   });
 
   ScrollTrigger.create({
-      trigger: "#date-id",
-      scroller: "#mobile-scroll",
-      onEnter: function () { changeBackground(".icon-date", "active") },
-      onEnterBack: function() { changeBackground(".icon-date", "active") },
-      onLeave: function () { changeBackground(".icon-date", "") },
-      onLeaveBack: function () { changeBackground(".icon-date", "") }
-    });
-  
-  ScrollTrigger.create({
-      trigger: "#love-id",
-      scroller: "#mobile-scroll",
-      onEnter: function () { changeBackground(".icon-love", "active") },
-      onEnterBack: function() { changeBackground(".icon-love", "active") },
-      onLeave: function () { changeBackground(".icon-love", "") },
-      onLeaveBack: function () { changeBackground(".icon-love", "") }
+    trigger: "#date-id",
+    scroller: "#mobile-scroll",
+    onEnter: function () {
+      changeBackground(".icon-date", "active");
+    },
+    onEnterBack: function () {
+      changeBackground(".icon-date", "active");
+    },
+    onLeave: function () {
+      changeBackground(".icon-date", "");
+    },
+    onLeaveBack: function () {
+      changeBackground(".icon-date", "");
+    },
   });
-  
+
   ScrollTrigger.create({
-      trigger: "#maps-id",
-      scroller: "#mobile-scroll",
-      onEnter: function () { changeBackground(".icon-location", "active") },
-      onEnterBack: function() { changeBackground(".icon-location", "active") },
-      onLeave: function () { changeBackground(".icon-location", "") },
-      onLeaveBack: function () { changeBackground(".icon-location", "") }
+    trigger: "#love-id",
+    scroller: "#mobile-scroll",
+    onEnter: function () {
+      changeBackground(".icon-love", "active");
+    },
+    onEnterBack: function () {
+      changeBackground(".icon-love", "active");
+    },
+    onLeave: function () {
+      changeBackground(".icon-love", "");
+    },
+    onLeaveBack: function () {
+      changeBackground(".icon-love", "");
+    },
   });
-  
+
   ScrollTrigger.create({
-      trigger: "#instagram-id",
-      scroller: "#mobile-scroll",
-      onEnter: function () { changeBackground(".icon-insta", "active") },
-      onEnterBack: function() { changeBackground(".icon-insta", "active") },
-      onLeave: function () { changeBackground(".icon-insta", "") },
-      onLeaveBack: function () { changeBackground(".icon-insta", "") }
+    trigger: "#maps-id",
+    scroller: "#mobile-scroll",
+    onEnter: function () {
+      changeBackground(".icon-location", "active");
+    },
+    onEnterBack: function () {
+      changeBackground(".icon-location", "active");
+    },
+    onLeave: function () {
+      changeBackground(".icon-location", "");
+    },
+    onLeaveBack: function () {
+      changeBackground(".icon-location", "");
+    },
   });
-  
+
   ScrollTrigger.create({
-      trigger: "#doa-id",
-      scroller: "#mobile-scroll",
-      onEnter: function () { changeBackground(".icon-doa", "active") },
-      onEnterBack: function() { changeBackground(".icon-doa", "active") },
-      onLeave: function () { changeBackground(".icon-doa", "") },
-      onLeaveBack: function () { changeBackground(".icon-doa", "") }
-    });
+    trigger: "#instagram-id",
+    scroller: "#mobile-scroll",
+    onEnter: function () {
+      changeBackground(".icon-insta", "active");
+    },
+    onEnterBack: function () {
+      changeBackground(".icon-insta", "active");
+    },
+    onLeave: function () {
+      changeBackground(".icon-insta", "");
+    },
+    onLeaveBack: function () {
+      changeBackground(".icon-insta", "");
+    },
+  });
+
+  ScrollTrigger.create({
+    trigger: "#doa-id",
+    scroller: "#mobile-scroll",
+    onEnter: function () {
+      changeBackground(".icon-doa", "active");
+    },
+    onEnterBack: function () {
+      changeBackground(".icon-doa", "active");
+    },
+    onLeave: function () {
+      changeBackground(".icon-doa", "");
+    },
+    onLeaveBack: function () {
+      changeBackground(".icon-doa", "");
+    },
+  });
 });
-
